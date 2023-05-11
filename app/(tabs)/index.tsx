7cb80d4b295af65c2f50220eb0a4aa3bbf0b6cd1
Attah David 'Joche <39128641+JocheAttah@users.noreply.github.com>
2023-05-11 03:07:30 +0100

    Fixed ui issue

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,25 @@
+import { FlatList, View } from 'react-native';
+
+import tweets from '../../assets/data/tweets';
 import Tweet from '../../components/Tweet';
-
-
-
 
 export default function TabOneScreen() {
   return (
-    <>
-    <Tweet/ >
-    <Tweet/ >
-    <Tweet/ >
-    </>
+    <View className='flex-1 bg-white' >
+    <FlatList
+        data={tweets}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+        showsVerticalScrollIndicator={false}
+      />
+    </ View>
   );
+}
+
+
+const renderItem = ({item}) => {
+  return (
+    <Tweet tweet={item} />
+  )
 }
 
--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -1,27 +1,29 @@
 import { View, Text, Image } from 'react-native'
 import React from 'react'
 
-import { EllipsisVerticalIcon } from 'react-native-heroicons/mini'
+import { EllipsisVerticalIcon } from 'react-native-heroicons/solid'
 import { ChatBubbleOvalLeftIcon, HeartIcon, ShareIcon } from 'react-native-heroicons/outline'
-import { EvilIcons } from '@expo/vector-icons'
+import { EvilIcons, Feather } from '@expo/vector-icons'
 import tweets from '../assets/data/tweets'
 
 const tweet = tweets[0];
 
 
-const Tweet = () => {
+const Tweet = ({tweet}) => {
   return (
-    <View className='bg-white flex flex-row justify-center items-start px-4 py-4 border-0 border-b-[0.3px] border-b-gray-400  '>
+    <View className='bg-white w-screen flex-1  flex-row items-start px-4 py-4 border-0 border-b-[0.2px] border-b-gray-300'>
       <Image src={tweet.user.image} className='w-[50px] h-[50px] rounded-[50px]' />
-      <View className='ml-2.5'>
+      <View className='pl-2 w-[88%]'>
         <View className='flex-row justify-between items-center mb-0.5'>
           <View className='flex-row'>
             <Text className='text-sm font-bold'>{tweet.user.name}</Text>
             <Text className='text-sm font-normal text-gray-500'> @{tweet.user.username}</Text>
           </View>
-          <EllipsisVerticalIcon size={20} color={'grey'} />
+          <View>
+              <EllipsisVerticalIcon  size={20} color={'grey'}  />
+          </View>
         </View>
-        <View>
+        <View className='w-full'>
           <Text>{tweet.content}</Text>
         </View>
         <View className='flex-row justify-between items-center pr-4 mt-2'>
